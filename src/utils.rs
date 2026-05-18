220a0dfb643d24ff8c744e6164aded072c373e64
Ryanu01 <ryangomes08090109@gmail.com>
2026-05-18 19:15:30 +0530

    Feat: added nested file completion

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, path::Path};
 
 pub fn longest_common_prefix (strings: &[String]) -> String {
     
@@ -24,13 +24,22 @@ pub fn longest_common_prefix (strings: &[String]) -> String {
 pub fn get_file_matches (prefix: &str) -> Vec<String> {
     let mut matches = Vec::new();
 
-    if let Ok(entries) = fs::read_dir(".") {
+    let (dir, partial) = match prefix.rfind('/') {
+        Some(idx) => (&prefix[..idx + 1], &prefix[idx + 1..]),
+        None => ("./", prefix)
+    };
+
+    let search_dir = Path::new(dir);
+
+    if let Ok(entries) = fs::read_dir(search_dir) {
         for entry in entries.flatten() {
             let file_name = entry.file_name();
 
-            if let Some(name) = file_name.to_str() {
-                if name.starts_with(prefix) {
-                    matches.push(name.to_string());
+            if let Some(name) = file_name.to_str() { 
+                if name.starts_with(partial) {
+                    let full_path = format!("{}{}", dir, name);
+                    let cleaned = full_path.strip_prefix("./").unwrap_or(&full_path);
+                    matches.push(cleaned.to_string());
                 }
             }
         }
